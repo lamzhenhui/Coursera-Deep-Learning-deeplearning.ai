f8cd3a5f718ebcf4fab3651f704329e446865ff7
lamzhenhui <lamzhenhui>
2024-08-13 12:02:11 +0800

    keras.api> keras

--- 04-Convolutional Neural Networks/week3/yad2k/models/keras_darknet19.py
+++ 04-Convolutional Neural Networks/week3/yad2k/models/keras_darknet19.py
@@ -2,10 +2,10 @@
 import functools
 from functools import partial
 
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.normalization import BatchNormalization
-from keras.models import Model
+from keras.api.layers import Conv2D, MaxPooling2D
+from keras.api.layers.advanced_activations import LeakyReLU
+from keras.api.layers import BatchNormalization
+from keras.api.models import Model
 from keras.regularizers import l2
 
 from ..utils import compose
--- 04-Convolutional Neural Networks/week3/yad2k/models/keras_yolo.py
+++ 04-Convolutional Neural Networks/week3/yad2k/models/keras_yolo.py
@@ -4,12 +4,13 @@ import sys
 import numpy as np
 import tensorflow as tf
 from keras import backend as K
-from keras.layers import Lambda
-from keras.layers.merge import concatenate
-from keras.models import Model
+from keras.api.layers import Lambda
+from keras.api.layers.merge import concatenate
+from keras.api.models import Model
 
 from ..utils import compose
-from .keras_darknet19 import (DarknetConv2D, DarknetConv2D_BN_Leaky, darknet_body)
+from .keras_darknet19 import (
+    DarknetConv2D, DarknetConv2D_BN_Leaky, darknet_body)
 
 sys.path.append('..')
 
@@ -102,13 +103,15 @@ def yolo_head(feats, anchors, num_classes):
 
     # TODO: Repeat_elements and tf.split doesn't support dynamic splits.
     # conv_width_index = K.repeat_elements(conv_width_index, conv_dims[1], axis=0)
-    conv_width_index = K.tile(K.expand_dims(conv_width_index, 0), [conv_dims[0], 1])
+    conv_width_index = K.tile(K.expand_dims(
+        conv_width_index, 0), [conv_dims[0], 1])
     conv_width_index = K.flatten(K.transpose(conv_width_index))
     conv_index = K.transpose(K.stack([conv_height_index, conv_width_index]))
     conv_index = K.reshape(conv_index, [1, conv_dims[0], conv_dims[1], 1, 2])
     conv_index = K.cast(conv_index, K.dtype(feats))
-    
-    feats = K.reshape(feats, [-1, conv_dims[0], conv_dims[1], num_anchors, num_classes + 5])
+
+    feats = K.reshape(
+        feats, [-1, conv_dims[0], conv_dims[1], num_anchors, num_classes + 5])
     conv_dims = K.cast(K.reshape(conv_dims, [1, 1, 1, 1, 2]), K.dtype(feats))
 
     # Static generation of conv_index:
@@ -328,7 +331,7 @@ def yolo_eval(yolo_outputs,
     boxes = yolo_boxes_to_corners(box_xy, box_wh)
     boxes, scores, classes = yolo_filter_boxes(
         box_confidence, boxes, box_class_probs, threshold=score_threshold)
-    
+
     # Scale boxes back to original image shape.
     height = image_shape[0]
     width = image_shape[1]
@@ -344,7 +347,7 @@ def yolo_eval(yolo_outputs,
     boxes = K.gather(boxes, nms_index)
     scores = K.gather(scores, nms_index)
     classes = K.gather(classes, nms_index)
-    
+
     return boxes, scores, classes
 
 
@@ -394,10 +397,10 @@ def preprocess_true_boxes(true_boxes, anchors, image_size):
         box = box[0:4] * np.array(
             [conv_width, conv_height, conv_width, conv_height])
         i = np.floor(box[1]).astype('int')
-        j = min(np.floor(box[0]).astype('int'),1)
+        j = min(np.floor(box[0]).astype('int'), 1)
         best_iou = 0
         best_anchor = 0
-                
+
         for k, anchor in enumerate(anchors):
             # Find IOU between box shifted to origin and anchor box.
             box_maxes = box[2:4] / 2.
@@ -415,7 +418,7 @@ def preprocess_true_boxes(true_boxes, anchors, image_size):
             if iou > best_iou:
                 best_iou = iou
                 best_anchor = k
-                
+
         if best_iou > 0:
             detectors_mask[i, j, best_anchor] = 1
             adjusted_box = np.array(
